7d107c476697652fe85be8c5e093a32f20639959
NuhDemir <drmaa217@gmail.com>
2024-07-04 20:45:33 +0300

    types

---
+++ to practice/Part_1/src/ch003/Types.java
@@ -0,0 +1,12 @@
+package ch003;
+
+public class Types {
+    //Error
+    //i= 6; //ı must have a type
+
+    int i = 1; //i must have a  type that is suitable for value 1
+    //i = true;
+    
+
+
+}
