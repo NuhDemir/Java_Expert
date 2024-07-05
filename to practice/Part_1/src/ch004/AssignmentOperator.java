4075148b3584acc274bbf5534661629c955a268e
NuhDemir <drmaa217@gmail.com>
2024-07-05 15:08:27 +0300

    new

---
+++ to practice/Part_1/src/ch004/AssignmentOperator.java
@@ -0,0 +1,9 @@
+package ch004;
+
+public class AssignmentOperator {
+
+    public static void main(String[] args) {
+        int i = 3;
+        //boolean b = (i = 4); //Can't do that: Type mismatch: cannot convert from int to boolean
+    }
+}
