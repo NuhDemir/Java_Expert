682350fe7779b6e3abd3b9d2c915bfc57c0b5802
NuhDemir <drmaa217@gmail.com>
2024-07-05 15:14:44 +0300

    binary promotion

---
+++ to practice/Part_1/src/ch004/BinaryPromotion.java
@@ -0,0 +1,14 @@
+package ch004;
+
+public class BinaryPromotion {
+
+    public static void main(String[] args) {
+        byte b1 = 5;
+        byte b2 = 8;
+
+        // byte b3 = b1 * b2; //compile error
+
+        byte b3 = (byte) (b1 * b2);
+        
+    }
+}
