2fbb41508f2295d8c14b0c7c2799b3a4176b9e7a
NuhDemir <drmaa217@gmail.com>
2024-07-05 15:15:45 +0300

    update binary promotion

--- to practice/Part_1/src/ch004/BinaryPromotion.java
+++ to practice/Part_1/src/ch004/BinaryPromotion.java
@@ -9,6 +9,8 @@ public class BinaryPromotion {
         // byte b3 = b1 * b2; //compile error
 
         byte b3 = (byte) (b1 * b2);
-        
+        byte b = 3;
+        // byte c = b>>> b;
+        byte c = (byte) (b>>>b);
     }
 }
