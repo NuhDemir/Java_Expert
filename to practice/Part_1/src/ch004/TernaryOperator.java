9fc367d454d0e612df3d2ce5fc14e9650f38da61
NuhDemir <drmaa217@gmail.com>
2024-07-05 19:16:40 +0300

    update ternary operator

--- to practice/Part_1/src/ch004/TernaryOperator.java
+++ to practice/Part_1/src/ch004/TernaryOperator.java
@@ -6,39 +6,54 @@ public class TernaryOperator {
 
     }
 
-    public static void basics()
-    {
-
-    }
-    public static void ifElseEquivalnce()
-    {
-
-    }
-    public static void evaluation()
-    {
-
-    }
-    public static String returnTrue()
-    {
+    public static void basics() {
 
     }
 
-    public static String returnFalse()
-    {
+    public static void ifElseEquivalnce() {
 
     }
 
-    private static void hard()
-    {
+    public static void evaluation() {
 
     }
 
-    private static void errors()
-    {
+    public static String returnTrue() {
+
+    }
+
+    public static String returnFalse() {
+
+    }
+
+    private static void hard() {
+        boolean b = false;
+
+        int x = (int) (Math.random() * 1000);
+        int y = (int) (Math.random() * 1000);
+        int z = (int) (Math.random() * 1000);
+        // x = 100;
+        //y=200;
+        //z= 300;
+
+        System.out.println("x: " + x + "y: " + y + "z: " + z);
+
+        b = x < y ? x < z ? true:false:false; //Either x is smaller than both y and z at the same time or x isnot smaller than y
+
+        b = (x<y ? (x<z ? true:false):false);
+        System.out.println(b);
+    }
+
+    private static void errors() {
         int i = 8;
-        int i = 11;
+        int j = 11;
         //(i<j) ? System.out.println(i): System.out.println(j);//Can't do this.
 
+        int x = 0;
+        int y;
+        int z;
+
+        //     (x == 2 ? y:z) = 5; //The left-hand side of an assignment must v
 
     }
 
