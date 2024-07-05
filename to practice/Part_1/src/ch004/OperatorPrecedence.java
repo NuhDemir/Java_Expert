2a2f9033143d24ba996c6758fc4dcfdafdb75046
NuhDemir <drmaa217@gmail.com>
2024-07-05 16:01:11 +0300

    update operator precedence

--- to practice/Part_1/src/ch004/OperatorPrecedence.java
+++ to practice/Part_1/src/ch004/OperatorPrecedence.java
@@ -2,27 +2,41 @@ package ch004;
 
 public class OperatorPrecedence {
     public static void main(String[] args) {
-int a = 6;
-int b = 5;
-int c = 10;
+        int a = 6;
+        int b = 5;
+        int c = 10;
 
-float rs = a+ ++b * c /a*b;
-System.out.println("rs: " +rs);
-System.out.println("b: "+ b);
+        float rs = a + ++b * c / a * b;
+        System.out.println("rs: " + rs);
+        System.out.println("b: " + b);
 
-rs = a + (++b)*((c/a)*b);
-System.out.println("rs: " +rs);
+        rs = a + (++b) * ((c / a) * b);
+        System.out.println("rs: " + rs);
 
         System.out.println("a: " + a + " b: " + b + " c: " + c);
         a += b += c;
         System.out.println("a: " + a + " b: " + b + " c: " + c);
 
-        System.out.println();
+        System.out.println("---------");
+int u = 3;
+//System.out.println("u: " + ++u+u--);
+System.out.println(++u + u--);
+System.out.println("u: " + u);
+        System.out.println("---------");
+
+        int i = 5;
+        //int k = ++i++;
+    int k = ++i + i++;
+    System.out.println("i: " + i + " k: " +k);
+
+    int x = 10, y=8,z=3;
+    System.out.println("x*y%z: " +x *y % z);
+
+
 
     }
 
-    public void print(String a, String b, String c)
-    {
-        System.out.println(a+ " " +b +" " + c);
+    public void print(String a, String b, String c) {
+        System.out.println(a + " " + b + " " + c);
     }
 }
