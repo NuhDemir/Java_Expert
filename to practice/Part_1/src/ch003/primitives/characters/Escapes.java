0d53e1aef82b5d369ff0491f229f2c278a71226e
NuhDemir <drmaa217@gmail.com>
2024-07-05 13:08:14 +0300

    Escapes

---
+++ to practice/Part_1/src/ch003/primitives/characters/Escapes.java
@@ -0,0 +1,14 @@
+package ch003.primitives.characters;
+
+public class Escapes {
+    public static void main(String[] args) {
+        System.out.println("Geriye boşluk: " + '\b');
+        System.out.println("Sekme: =>" + '\t' + "<=");
+        System.out.println("Satır başı: " + '\n');
+        System.out.println("Form beslemesi: " +'\f');
+        System.out.println("Satır başı: " +'\r');
+        System.out.println("Apostrof işareti: " +'\'');
+        System.out.println("Tırnak işareti: " + "\"Ali'nin\"");
+        System.out.println("Ters eğik çizgi: " + '\\');
+    }
+}
