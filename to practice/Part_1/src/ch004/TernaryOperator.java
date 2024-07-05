00051078e6a7fba02eb367bf2f94d98e494f2701
NuhDemir <drmaa217@gmail.com>
2024-07-05 19:07:07 +0300

    ternary operator

---
+++ to practice/Part_1/src/ch004/TernaryOperator.java
@@ -0,0 +1,46 @@
+package ch004;
+
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void basics()
+    {
+
+    }
+    public static void ifElseEquivalnce()
+    {
+
+    }
+    public static void evaluation()
+    {
+
+    }
+    public static String returnTrue()
+    {
+
+    }
+
+    public static String returnFalse()
+    {
+
+    }
+
+    private static void hard()
+    {
+
+    }
+
+    private static void errors()
+    {
+        int i = 8;
+        int i = 11;
+        //(i<j) ? System.out.println(i): System.out.println(j);//Can't do this.
+
+
+    }
+
+
+}
