7c01374630936f93c131212d8cb4a8b2b9489a36
NuhDemir <drmaa217@gmail.com>
2024-07-05 19:58:56 +0300

    update unary ınc dec operator

--- to practice/Part_1/src/ch004/UnaryIncAndDecOperators.java
+++ to practice/Part_1/src/ch004/UnaryIncAndDecOperators.java
@@ -7,49 +7,70 @@ public class UnaryIncAndDecOperators {
 
     }
 
-    public static void unaryIncrementAndDecrement1()
-    {
+    public static void unaryIncrementAndDecrement1() {
+        int i = 1;
+        prt("i   : \t" + i);
+        prt("++i : \t" + ++i); // Pre-increment
+        prt("i++ : \t" + i++); // Post-increment
+        prt("i   : \t" + i);
+        prt("--i : \t" + --i); // Pre-decrement
+        prt("i-- : \t" + i--); // Post-decrement
+        prt("i   : \t" + i);
+
+        System.out.println();
+
+        i = 1;
+        prt("i: \t" + i);
+        int j = ++i;
+        prt("i: \t" + i + "\t j : \t" + j); // Pre-increment
+        j = i++;
+        prt("i: \t" + i + "\t j : \t" + j); // Post-increment
+        prt("i: \t" + i);
+        j = --i;
+        prt("i: \t" + i + "\t j : \t" + j); // Pre-decrement
+        j = i--;
+        prt("i: \t" + i + "\t j : \t" + j); // Post-decrement
+        prt("i: \t" + i);
+    }
+
+    public static void unaryIncrementAndDecrement2() {
 
     }
 
-    public static void unaryIncrementAndDecrement2()
-    {
-
-    }
-
-    public static void weirdBehavior()
-    {
-int i = 0;
+    public static void weirdBehavior() {
+        int i = 0;
         System.out.println("That's OK");
         i = ++i;
-        System.out.println("i: "+ i);
-        i = ++i +2;
-        System.out.println("i: " +i);
+        System.out.println("i: " + i);
+        i = ++i + 2;
+        System.out.println("i: " + i);
 
         i = 0;
         System.out.println("Taht's not OK");
         i = i++;
 
-        System.out.println("i: " +i);//Why doesn't i increase
+        System.out.println("i: " + i);//Why doesn't i increase
 
-        i = i++ +2; //Why doesn't i++ increase i?
-        System.out.println("i: " +i);
+        i = i++ + 2; //Why doesn't i++ increase i?
+        System.out.println("i: " + i);
 
         System.out.println("That's OK too.");
 
-        i= 0;
+        i = 0;
 
         int j = i++;
-        System.out.println("i: " +i);
-        System.out.println("j: " +j);
+        System.out.println("i: " + i);
+        System.out.println("j: " + j);
+
+        int k = i++;
+        System.out.println("i: " + i);
+        System.out.println("k: " + k);
 
     }
 
-    static void prt(String s)
-    {
+    static void prt(String s) {
         System.out.println(s);
     }
-
 
 
 }
