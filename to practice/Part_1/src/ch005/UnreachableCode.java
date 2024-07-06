a394ada7d1e38d89447398b63a630fb5e3f0bfa0
NuhDemir <drmaa217@gmail.com>
2024-07-06 20:39:29 +0300

    update unreachable code

--- to practice/Part_1/src/ch005/UnreachableCode.java
+++ to practice/Part_1/src/ch005/UnreachableCode.java
@@ -2,7 +2,7 @@ package ch005;
 
 public class UnreachableCode {
     public static void main(String[] args) {
-        //not an unreachable code
+        // Ulaşılabilir kod
         int a = 8;
         int k;
         if (a < 11)
@@ -10,65 +10,75 @@ public class UnreachableCode {
         else
             k = 9;
 
-        System.out.println(k);
+        System.out.println(k); // Bu kod çalışır çünkü k değişkeni her durumda bir değer alır.
 
+        // Sonsuz döngü, bir noktada break ile sonlanırsa bu kod çalışır.
         while (true) {
             System.out.println("Hello :)");
             if (k > 3)
-                break;
+                break; // break ifadesi döngüyü sonlandırır.
         }
-        int ii = 5; // Unreachable code without break.
+
+        // Bu kod unreachable olurdu eğer döngü sonsuz olsaydı ve break olmasaydı.
+        int ii = 5;
 
         int i = 5;
-        //Unreachable code
 
-        while (false)
-            System.out.println("Selam :)");
+        // Ulaşılmaz kod
+    //    while (false) // Bu döngü asla çalışmaz.
+      //      System.out.println("Selam :)");
 
-        int r = 5;
+        //int r = 5;
 
         if (false) {
+            // Bu blok asla çalışmaz.
             System.out.println("SLE");
         }
-        for (int m = 0; false; m++) {
-//			System.out.println(m);
-//			break;
-//		}
-//		System.out.println("Break!"); // Unreachable code without a break
-//
-//		for (;;) {
-//			System.out.println("Selam");
-////			break;
-//			System.out.println("Bir selam daha."); // Unreachable code due to break
-//		}
-//
-////		for (;;) {
-////			System.out.println("Selam");
-////			if (true)
-////				break;
-////			System.out.println("Bir selam daha.");   // Dead code not unreachable code
-////		}
-//
-////		 try {
-////
-////		 } catch (IOException e) { // Unreachable code. Try to catch Exception
-////		 // or RuntimeException and see what happens
-////
-////		 }
-//
-//		// return;
-//		// System.out.println("Son after return"); // Unreachable code!
-//	}
-//
-//	public int f() {
-//		if (false) {
-//			System.out.println("In if");
-//			return 8;
-//		} else {
-//			System.out.println("in Else");
-//			return 0;
-//		}
-            // System.out.println("Son"); // Unreachable code!
-//	}
+
+      //  for (int m = 0; false; m++) {
+            // Bu döngü asla çalışmaz.
+//            System.out.println(m);
+//            break;
         }
+
+        // Bu kod unreachable olurdu eğer döngü sonsuz olsaydı ve break olmasaydı.
+//        System.out.println("Break!");
+
+//        for (;;) {
+//            System.out.println("Selam");
+//            break;
+//            System.out.println("Bir selam daha."); // break ile unreachable olur
+//        }
+
+//        for (;;) {
+//            System.out.println("Selam");
+//            if (true)
+//                break;
+//            System.out.println("Bir selam daha.");   // break ile çalışmaz, dead code değil unreachable code
+//        }
+
+//        try {
+//
+//        } catch (IOException e) { // Unreachable code. Try to catch Exception
+//            // or RuntimeException and see what happens
+//
+//        }
+
+        // return sonrası kod unreachable olur.
+//        return;
+//        System.out.println("Son after return"); // Unreachable code!
     }
+
+  //  public int f() {
+   //     if (false) {
+  //          System.out.println("In if");
+    //        return 8;
+      //  } else {
+        //    System.out.println("in Else");
+          //  return 0;
+       // }
+        // return sonrası kod unreachable olur.
+        // System.out.println("Son"); // Unreachable code!
+//    }
+//}
+//}
