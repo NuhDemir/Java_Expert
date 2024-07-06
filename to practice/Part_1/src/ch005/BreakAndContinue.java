18bf345377dcc8f7ed95bddbf37cbeceb653ccca
NuhDemir <drmaa217@gmail.com>
2024-07-06 15:47:14 +0300

    break and continue

---
+++ to practice/Part_1/src/ch005/BreakAndContinue.java
@@ -0,0 +1,10 @@
+package ch005;
+
+public class BreakAndContinue {
+
+    public static void main(String[] args) {
+        System.out.println("Break: ");
+        for(int i = 1; i<=10; i++)
+        {
+    }
+}
