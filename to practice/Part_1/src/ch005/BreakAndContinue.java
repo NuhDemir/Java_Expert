cb2ec9fa0138a77d8775be2d2ac00a3ab865d9ce
NuhDemir <drmaa217@gmail.com>
2024-07-06 15:50:41 +0300

    update break and continue

--- to practice/Part_1/src/ch005/BreakAndContinue.java
+++ to practice/Part_1/src/ch005/BreakAndContinue.java
@@ -4,7 +4,19 @@ public class BreakAndContinue {
 
     public static void main(String[] args) {
         System.out.println("Break: ");
-        for(int i = 1; i<=10; i++)
+        for (int i = 1; i <= 10; i++) {
+            if (i%2 ==0)
+                break;//Terminated loop
+            System.out.println(i + "\t" +Math.sqrt(i));
+        }
+
+        System.out.println("\nContinue: ");
+
+        for (int i = 1; i <=10;i++)
         {
+            if (i%2 ==0)
+                continue;//Terminated only this step.
+            System.out.println(i + "\t" +Math.sqrt(i));
+        }//end of for loop
     }
 }
