44889987172270e5c20bec28ebf7adc602be19de
NuhDemir <drmaa217@gmail.com>
2024-07-07 16:20:10 +0300

    stack over flow

---
+++ to practice/Part_1/src/ch005/algorithm/StackOverflow.java
@@ -0,0 +1,16 @@
+package ch005.algorithm;
+
+public class StackOverflow {
+    private static int count;
+
+    public static void main(String[] args) {
+        f();
+    }
+
+    public static void f() {
+        count++;
+        if (count % 100 == 0)
+            System.out.println("count: " + count);
+        f();
+    }
+}
