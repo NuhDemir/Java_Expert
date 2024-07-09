bb0ec0eaef5ecfbe758fcd95c4cbaeee6de6a87a
NuhDemir <drmaa217@gmail.com>
2024-07-09 20:25:43 +0300

    employee

--- to exercises/Exercises/src/ch001/App.java
+++ to exercises/Exercises/src/ch001/App.java
@@ -1,4 +1,26 @@
 package ch001;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class App {
+    public static void main(String[] args) throws Exception {
+
+        List<Integer> numbers = Arrays.asList(60,50,80,70,90,30);
+       writeList(numbers);
+
+        Collections.sort(numbers);
+        System.out.println("---");
+       writeList(numbers);
+    }
+
+private static void writeList(List<Integer> collection)
+{
+    for (Integer item: collection)
+    {
+        System.out.println(item);
+    }
+}
+
 }
--- to exercises/Exercises/src/ch001/Employee.java
+++ to exercises/Exercises/src/ch001/Employee.java
@@ -1,4 +1,42 @@
 package ch001;
 
+import java.security.PublicKey;
+
 public class Employee {
+    private String fullName;
+    private Double salary;
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Employee()
+    {
+
+    }
+
+
+    public Employee(String fullName,Double salary)
+    {
+        this.fullName = fullName;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return getFullName() +" " +getSalary();
+    }
 }
