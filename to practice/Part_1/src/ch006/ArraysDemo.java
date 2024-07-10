0515f1d84ea6b7c5dec6a2390f8f90627d02ce97
NuhDemir <drmaa217@gmail.com>
2024-07-10 12:33:53 +0300

    array demo

---
+++ to practice/Part_1/src/ch006/ArraysDemo.java
@@ -0,0 +1,46 @@
+package ch006;
+
+import java.util.Arrays;
+
+public class ArraysDemo {
+
+    int[]  intArray = new int[20];
+    Pizza[] pizzas = new Pizza[4];
+    boolean[] flags = {true,false};
+
+    public static void main(String[] args) {
+        ArraysDemo demo = new ArraysDemo();
+        demo.initialize();
+        demo.copy();
+        demo.copyRange();
+        demo.fill();
+        demo.sort();
+        demo.search();
+        
+    }
+
+    private void search() {
+    }
+
+    private void sort() {
+        
+    }
+
+    private void fill() {
+        
+    }
+
+    private void copy() {
+        
+    }
+
+    private void copyRange() {
+        
+    }
+
+    private void initialize() {
+        
+    }
+
+
+}
