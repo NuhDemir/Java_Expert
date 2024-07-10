4704be7f65d0c65712b5b08db378955380b79281
NuhDemir <drmaa217@gmail.com>
2024-07-10 12:41:25 +0300

    copy and copy range

--- to practice/Part_1/src/ch006/ArraysDemo.java
+++ to practice/Part_1/src/ch006/ArraysDemo.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class ArraysDemo {
 
-    int[]  intArray = new int[20];
+    int[] intArray = new int[20];
     Pizza[] pizzas = new Pizza[4];
-    boolean[] flags = {true,false};
+    boolean[] flags = {true, false};
 
     public static void main(String[] args) {
         ArraysDemo demo = new ArraysDemo();
@@ -16,31 +16,47 @@ public class ArraysDemo {
         demo.fill();
         demo.sort();
         demo.search();
-        
+
     }
 
-    private void search() {
+    public void search() {
     }
 
-    private void sort() {
-        
+    public void sort() {
+
     }
 
-    private void fill() {
-        
+    public void fill() {
+
     }
 
-    private void copy() {
-        
+    public void copy() {
+        System.out.println("\nCopying the array: ");
+        int[] copy1 = Arrays.copyOf(intArray, 5);
+        System.out.println("Whole copy: ");
+        print(copy1);
+
+        System.out.print("Only 0 to 4 copy: ");
+        int[] copy2 = Arrays.copyOf(intArray,5);
+        print(copy2);
     }
 
-    private void copyRange() {
-        
+    public void copyRange() {
+System.out.println("\nCopying the range of the array: ");
+int[] copy = Arrays.copyOfRange(intArray,4,12);
+System.out.println("Range 5 to  15 copy: ");
+print(copy);
     }
 
-    private void initialize() {
-        
+    public void initialize() {
+
     }
 
+    public void print(int[] intArray) {
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i] + " - ");
+        }
+        System.out.println("---");
+    }
 
 }
