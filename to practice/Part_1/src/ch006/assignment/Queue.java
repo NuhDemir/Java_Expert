63c84e96f624fda093f3220e33cbf3408e3c0711
NuhDemir <drmaa217@gmail.com>
2024-07-11 19:52:33 +0300

    queue

---
+++ to practice/Part_1/src/ch006/assignment/Queue.java
@@ -0,0 +1,40 @@
+package ch006.assignment;
+
+public class Queue {
+    public static int MAX_QUEUE_SIZE=5;
+
+    public boolean queue(String newElement)
+    {
+        return false;
+    }
+
+    //Pop element from the top
+    public String dequeue()
+    {
+        String lastItem = null;
+        return  lastItem;
+
+    }
+
+    private void rearrange()
+    {
+
+    }
+
+    //Remove all elements from queue
+    public void clear()
+    {
+
+    }
+
+    //Capacity of queue
+    public int getCapacity()
+    {
+        return MAX_QUEUE_SIZE;
+    }
+
+    public void showElements()
+    {
+        
+    }
+}
