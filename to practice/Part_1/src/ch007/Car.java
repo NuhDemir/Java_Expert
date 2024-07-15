a875da1f89026f260cf197fe905a991f2bf187de
NuhDemir <drmaa217@gmail.com>
2024-07-15 20:42:57 +0300

    car ınherıtance

--- to practice/Part_1/src/ch007/Car.java
+++ to practice/Part_1/src/ch007/Car.java
@@ -9,4 +9,32 @@ public class Car {
     public String year;
     public int speed;
     public int distance;
+
+    public double go (int newDistance)
+    {
+        distance += newDistance;
+        double period = (double)newDistance /speed;
+        return period;
+    }
+
+    public void accelerate(int newSpeed)
+    {
+        speed = newSpeed;
+
+    }
+
+    public void stop()
+    {
+        speed= 0;
+    }
+
+    public String getInfo()
+    {
+        String info ="Car Info: " +year +" " +make + " " +model +" " +". Distance: " +" km. and traveling at " + speed + " kmph.";
+        return info;
+    }
+
+
+
+
 }
