c1c6d686144e8b34eab3837f1862dd4939f16359
NuhDemir <drmaa217@gmail.com>
2024-07-15 16:38:35 +0300

    java ch007

---
+++ to practice/Part_1/src/ch007/Car.java
@@ -0,0 +1,12 @@
+package ch007;
+
+import java.security.PublicKey;
+
+public class Car {
+
+    public String make;
+    public String model;
+    public String year;
+    public int speed;
+    public int distance;
+}
