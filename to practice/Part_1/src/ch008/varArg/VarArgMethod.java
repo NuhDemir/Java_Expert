255dee56ed637d6a1c866dd81807838221419ac9
NuhDemir <drmaa217@gmail.com>
2024-07-16 14:29:49 +0300

    update var arg method

--- to practice/Part_1/src/ch008/varArg/VarArgMethod.java
+++ to practice/Part_1/src/ch008/varArg/VarArgMethod.java
@@ -8,17 +8,46 @@ public class VarArgMethod {
     }
 
     //Method with two arguments
-    public static void  calculateAverage(int x,int y)
-    {
-        System.out.println("Average: " + (double)(x+y)/2);
+    public static void calculateAverage(int x, int y) {
+        System.out.println("Average: " + (double) (x + y) / 2);
     }
 
     //Method with three arguments and goes on!
-public static void calculateAverage(int x,int y,int z)
-{
-    System.out.println("Average: " +(double)(x+y+z)/3);
-}
+    public static void calculateAverage(int x, int y, int z) {
+        System.out.println("Average: " + (double) (x + y + z) / 3);
+    }
 
-//
+    //Method with array arguments. but it needs to be called by providing an array
+    public static void calculateAverageWithArray(int[] array) {
+        int sum = 0;
+        int i = 0;
+        for (int x : array) {
+            sum += x;
+            i++;
+        }
+        System.out.println("Average: " + (double) sum / i);
+    }
 
+    //That's the solutions.
+    public static void average(int... array) {
+        int sum = 0;
+        int x = 0;
+        for (int i : array) {
+            sum += i;
+            x++;
+        }
+
+        System.out.println("Average: " + (double) sum / x);
+    }
+
+    public static void anotherAverage(int y, boolean b, int... array) {
+        int sum = 0;
+        int i = 0;
+        for (int x : array) {
+            sum += x;
+            i++;
+        }
+        System.out.println("Average: " + (double) sum / i);
+        System.out.println("y: " + y);
+    }
 }
