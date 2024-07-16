27021d9ab118e751ad705d009433bc6968fc52f0
NuhDemir <drmaa217@gmail.com>
2024-07-16 14:04:14 +0300

    window

---
+++ to practice/Part_1/src/ch008/Window.java
@@ -0,0 +1,23 @@
+package ch008;
+
+public class Window {
+
+    boolean open;
+    int width;
+    int height;
+
+    public  boolean isOpen()
+    {
+        return open;
+    }
+
+    public void setOpen(booelan open)
+    {
+        this.open = open;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+}
