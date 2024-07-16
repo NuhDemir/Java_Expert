7274fba899fbd1ebbf0aa29760c043acd0d7cc2c
NuhDemir <drmaa217@gmail.com>
2024-07-16 17:26:42 +0300

    comment line to calculator overloaded

--- to practice/Part_1/src/ch008/overloading/CalculatorOverloaded.java
+++ to practice/Part_1/src/ch008/overloading/CalculatorOverloaded.java
@@ -5,34 +5,42 @@ public class CalculatorOverloaded {
         print("arguments are in byte");
         return first + second;
     }
+
     int add(short first, short second) {
         print("arguments are in short");
         return first + second;
     }
+
     int add(char first, char second) {
         print("arguments are in char");
         return first + second;
     }
+
     int add(int first, int second) {
         print("arguments are in int");
         return first + second;
     }
+
     long add(long first, long second) {
         print("arguments are in long");
         return first + second; // Sonucu döndür
     }
+
     long add(short first, long second) {
         print("arguments are in short");
         return first + second; // Sonucu döndür
     }
+
     float add(float first, float second) {
         print("arguments are in float");
         return first + second; // Sonucu döndür
     }
+
     double add(double first, double second) {
         print("arguments are in double");
         return first + second; // Sonucu döndür
     }
+
     // print metodunu tanımlıyoruz
     public static void print(String s) {
         System.out.println(s);
@@ -43,18 +51,20 @@ public class CalculatorOverloaded {
         byte b1 = 11;
         byte b2 = -42;
 
-        System.out.println(calculator.add(b1,b2));
+        // Byte değerleri toplama
+        print("Result of add(byte, byte): " + calculator.add(b1, b2));
 
         double d1 = 3.1415;
         double d2 = 2.990;
-        System.out.println(calculator.add(d1,d2));
+        // Double değerleri toplama
+        print("Result of add(double, double): " + calculator.add(d1, d2));
+
         byte s = 33;
         long l1 = 4;
-        System.out.println(calculator.add(s, l1));
+        // Short ve long değerleri toplama
+        print("Result of add(short, long): " + calculator.add(s, l1));
 
-        double d3 = 3.1415;
-        long l2 = 4;
-        System.out.println(calculator.add(d1, l2));
-
+        // Double ve long değerleri toplama
+        print("Result of add(double, long): " + calculator.add(d1, l2));
     }
 }
