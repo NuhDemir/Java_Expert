1ea1aa44bed3bec46e770cee57176e269a924617
NuhDemir <drmaa217@gmail.com>
2024-07-16 16:08:51 +0300

    comment line to update primitive overloading

--- to practice/Part_1/src/ch008/overloading/PrimitiveOverloading2.java
+++ to practice/Part_1/src/ch008/overloading/PrimitiveOverloading2.java
@@ -2,18 +2,26 @@ package ch008.overloading;
 
 public class PrimitiveOverloading2 {
     public static void main(String[] args) {
+        // byte türünde iki değişken tanımlanıyor
         byte b1 = 1;
         byte b2 = 33;
-print(b1,b2);
-    }
-public static void print(int i,short j){
-    System.out.println("print(short i,short j)");
-}
-public static void print(short i,int j){
-        System.out.println("print(short i,int j)");
-}
 
-    public static void print(short i,short j){
-        System.out.println("print(short i,short j)");
+        // 'print' metodu çağrılıyor
+        print(b1, b2); // Burada byte türündeki değişkenler gönderiliyor
+    }
+
+    // int ve short türünde parametre alan 'print' metodu
+    public static void print(int i, short j) {
+        System.out.println("print(int i, short j)");
+    }
+
+    // short ve int türünde parametre alan 'print' metodu
+    public static void print(short i, int j) {
+        System.out.println("print(short i, int j)");
+    }
+
+    // İki short türünde parametre alan 'print' metodu
+    public static void print(short i, short j) {
+        System.out.println("print(short i, short j)");
     }
 }
