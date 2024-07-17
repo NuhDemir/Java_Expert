2a0a256b9c1a1965d05be1a8725f1c8fa672044a
NuhDemir <drmaa217@gmail.com>
2024-07-17 17:11:04 +0300

    overloading order

---
+++ to practice/Part_1/src/ch008/housekeeping/methodOverloading/OverloadingOrder.java
@@ -0,0 +1,16 @@
+package ch008.housekeeping.methodOverloading;
+
+public class OverloadingOrder {
+    static void f(String s, int i) {
+        System.out.println("String: " + s + ", int: " + i);
+    }
+
+    static void f(int i, String s) {
+        System.out.println("int: " + i + ", String: " + s);
+    }
+
+    public static void main(String[] args) {
+        f("String first", 21);
+        f(34, "Int first");
+    }
+}
