33da43141db65544ffe83bf8e720e6bae340b2d2
NuhDemir <drmaa217@gmail.com>
2024-07-18 19:09:41 +0300

    init values

---
+++ to practice/Part_1/src/ch008/housekeeping/InitialValues2.java
@@ -0,0 +1,13 @@
+package ch008.housekeeping;
+
+public class InitialValues2 {
+    boolean aBoolean = true;
+    char ch = 'c';
+    byte b = 22;
+    short s = 0xff;
+    int i = 888;
+    long l = 1;
+    float f = 3.14F;
+    double d = 3.14159d;
+
+}
