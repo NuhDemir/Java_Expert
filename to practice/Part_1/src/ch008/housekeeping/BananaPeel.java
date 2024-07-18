7218833c1fbfe8f0920333f541d65af9853cf86d
NuhDemir <drmaa217@gmail.com>
2024-07-18 14:20:06 +0300

    banana peel

---
+++ to practice/Part_1/src/ch008/housekeeping/BananaPeel.java
@@ -0,0 +1,18 @@
+package ch008.housekeeping;
+
+class Banana {
+    void peel(int i) {
+        /*..........*/
+        /*..........*/
+    }
+}
+
+public class BananaPeel {
+    public static void main(String[] args) {
+        Banana a = new Banana(),
+        b = new Banana();
+        a.peel(1);
+        b.peel(2);
+
+    }
+}
