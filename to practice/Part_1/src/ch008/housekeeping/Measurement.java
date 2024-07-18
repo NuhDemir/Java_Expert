bbfc73847ff0c7679d1c695ae78ac6283a673dd7
NuhDemir <drmaa217@gmail.com>
2024-07-18 19:09:52 +0300

    measurement

---
+++ to practice/Part_1/src/ch008/housekeeping/Measurement.java
@@ -0,0 +1,14 @@
+package ch008.housekeeping;
+
+// Derinlik sınıfı
+class Depth {
+    // Bu sınıf şu anda herhangi bir özellik veya metod içermiyor.
+}
+
+// Ölçüm sınıfı
+public class Measurement {
+    // Measurement sınıfı içinde bir Depth nesnesi oluşturuluyor
+    Depth d = new Depth();
+    /* .......... */
+    // Buraya başka özellikler ve metodlar eklenebilir
+}
