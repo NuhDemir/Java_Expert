71995a620948d91680c07692940c6155f1238ccb
NuhDemir <drmaa217@gmail.com>
2024-07-20 21:29:42 +0300

    apple toss

---
+++ to exercises/Exercises/src/ch005/AppleToss.java
@@ -0,0 +1,22 @@
+package ch005;
+
+import ch001.App;
+
+import javax.swing.*;
+import java.lang.reflect.Field;
+
+public class AppleToss extends JFrame {
+
+    public AppleToss() {
+
+    }
+
+    private void setUpFieldForOnePlayer() {
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
