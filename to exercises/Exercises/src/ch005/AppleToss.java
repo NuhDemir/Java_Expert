1effb8b0834dfe2f514252fd3e7ddc24247f92c8
NuhDemir <drmaa217@gmail.com>
2024-07-20 22:53:36 +0300

    update apple toss

--- to exercises/Exercises/src/ch005/AppleToss.java
+++ to exercises/Exercises/src/ch005/AppleToss.java
@@ -3,20 +3,34 @@ package ch005;
 import ch001.App;
 
 import javax.swing.*;
-import java.lang.reflect.Field;
 
 public class AppleToss extends JFrame {
-
+Field field = new Field();
+Physicist player1 = new Physicist();
     public AppleToss() {
+//Create our frame
+        super("Apple Toss Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setResizable(false);
 
+        setUpFieldForOnePlayer();
     }
 
     private void setUpFieldForOnePlayer() {
-
+        // Place our (new) physicist in the lower left corner and connect them to the field
+        // Place our (new) physicist in the lower left corner and connect them to the field
+        player1.setPosition(100,500);
+        field.setPlayer(player1);
+        player1.setField(field);
+        field.setUpApples();
+        field.setUpTree();
+        add(field);
 
     }
 
     public static void main(String[] args) {
-
+AppleToss game = new AppleToss();
+game.setVisible(true);
     }
 }
