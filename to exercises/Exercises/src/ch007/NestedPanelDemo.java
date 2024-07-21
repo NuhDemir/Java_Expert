02de82a58c1d1db2bd5ebc7ac662ee251d7a504c
NuhDemir <drmaa217@gmail.com>
2024-07-21 17:39:41 +0300

    nested panel

--- to exercises/Exercises/src/ch007/NestedPanelDemo.java
+++ to exercises/Exercises/src/ch007/NestedPanelDemo.java
@@ -9,13 +9,26 @@ public class NestedPanelDemo {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Nested Panel");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400,600);
+        frame.setSize(400, 600);
 
 
         JTextArea messageArea = new JTextArea();
         frame.add(messageArea, BorderLayout.CENTER);
 
+        JPanel buttonPanel = new JPanel(new GridLayout(1, 0));
+        JButton sendButton = new JButton("Send");
+        JButton saveButton = new JButton("Save");
+        JButton resetButton = new JButton("Reset");
+        JButton cancelButton = new JButton("Cancel");
 
+
+        buttonPanel.add(sendButton);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(resetButton);
+        buttonPanel.add(cancelButton);
+
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+        frame.setVisible(true);
 
     }
 }
