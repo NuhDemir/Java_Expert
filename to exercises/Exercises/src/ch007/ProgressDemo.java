8883d400c8c3832459e876e5fe6371e979da9e2f
NuhDemir <drmaa217@gmail.com>
2024-07-21 11:36:32 +0300

    progress demo

---
+++ to exercises/Exercises/src/ch007/ProgressDemo.java
@@ -0,0 +1,46 @@
+package ch007;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ProgressDemo {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("swing utilities demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+        frame.setSize(500, 500);
+
+        JLabel label = new JLabel("Download Progress", JLabel.CENTER);
+        Thread pretender = new Thread(new ProgressPretender(label));
+    }
+}
+
+class ProgressPretender implements Runnable {
+    JLabel label;
+    int progress;
+
+    public ProgressPretender(JLabel label) {
+        this.label = label;
+        progress = 0;
+    }
+
+    public void run() {
+
+        while (progress <= 100) {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    label.setText(progress + "%");
+                }
+            });
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException interruptedException) {
+                System.err.println("Someone interrupted us.");
+                break;
+            }
+            progress++;
+        }
+
+    }
+}
