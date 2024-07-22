64b8659127bdfce6d79d7eec65a547e45cbba933
NuhDemir <drmaa217@gmail.com>
2024-07-22 20:06:38 +0300

    optional trailing arguments

---
+++ to practice/Part_1/src/ch009/OptionalTrailingArguments.java
@@ -0,0 +1,22 @@
+package ch009;
+
+public class OptionalTrailingArguments {
+
+    static void f(int required, String... trailing) {
+        System.out.print("required" + required + " ");
+
+        for (String s : trailing) {
+            System.out.print(s + " ");
+
+        }
+        System.out.println("-------");
+    }
+
+    public static void main(String[] args) {
+        f(1, "one");
+        f(2, "two", "three");
+        f(0);
+        f(-1, "two");
+
+    }
+}
