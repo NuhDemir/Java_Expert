0bda9a3254ba25314ddda992e42a15d773295cd5
NuhDemir <drmaa217@gmail.com>
2024-07-22 20:06:03 +0300

    simple enum use

---
+++ to practice/Part_1/src/ch009/enumerated_types/SimpleEnumUse.java
@@ -0,0 +1,8 @@
+package ch009.enumerated_types;
+
+public class SimpleEnumUse {
+    public static void main(String[] args) {
+        Spiciness attack = Spiciness.ATTACK;
+        System.out.println(attack);
+    }
+}
