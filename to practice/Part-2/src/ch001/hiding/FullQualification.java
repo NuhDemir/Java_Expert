d2673f0c5ff3401fc511d4478549480298f8976f
NuhDemir <drmaa217@gmail.com>
2024-07-23 18:56:21 +0300

    Full Qualification

---
+++ to practice/Part-2/src/ch001/hiding/FullQualification.java
@@ -0,0 +1,8 @@
+package ch001.hiding;
+
+//bunun yerine 'import anahtar' sözcüğünü kullanacağız
+public class FullQualification {
+    public static void main(String[] args) {
+        java.util.ArrayList list = new java.util.ArrayList<>();
+    }
+}
