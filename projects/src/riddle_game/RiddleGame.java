790f5fb8a262eae42379b50cf260253545d3bb5c
NuhDemir <drmaa217@gmail.com>
2024-07-23 20:51:43 +0300

    update riddle game

--- projects/src/riddle_game/RiddleGame.java
+++ projects/src/riddle_game/RiddleGame.java
@@ -1,5 +1,7 @@
 package riddle_game;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RiddleGame {
@@ -42,37 +44,46 @@ public class RiddleGame {
         };
 
         int numRiddles = riddles.length;
+        List<Integer> askedRiddles = new ArrayList<>();
 
-        while (true) {
+        while (askedRiddles.size() < numRiddles) {
             int randomIndex = (int) (Math.random() * numRiddles);
-            Riddle selectRiddle = riddles[randomIndex];
 
-            //Kullanıcıya bulmacayı göster.
-            System.out.println("Bulmaca" + selectRiddle.getQuestion());
+            // Daha önce sorulmuş bir bulmaca mı kontrol et
+            if (askedRiddles.contains(randomIndex)) {
+                continue;
+            }
 
-            //Kullanıcının cevabını alın.
+            Riddle selectedRiddle = riddles[randomIndex];
+            askedRiddles.add(randomIndex);
+
+            // Kullanıcıya bulmacayı göster
+            System.out.println("Bulmaca:\n " + selectedRiddle.getQuestion());
+
+            // Kullanıcının cevabını alın
             String userAnswer = scanner.nextLine();
 
-            if (userAnswer.equalsIgnoreCase(selectRiddle.getAnswer())) {
+            if (userAnswer.equalsIgnoreCase(selectedRiddle.getAnswer())) {
                 System.out.println("Doğru cevap!");
                 score++;
             } else {
-                //Eğer cevap yanlışsa ,ipucu teklif edin
+                // Eğer cevap yanlışsa, ipucu teklif edin
                 System.out.println("Yanlış! İpucu ister misiniz? (evet/hayır)");
                 String giveHint = scanner.nextLine().toLowerCase();
                 if (giveHint.equals("evet")) {
-                    System.out.println("İpucu: " + selectRiddle.getHint());
+                    System.out.println("İpucu: " + selectedRiddle.getHint());
                 }
             }
 
-            //Kullanıcının devam edip etmeyeceğini soralım
+            // Kullanıcının devam edip etmeyeceğini sor
             System.out.println("Oyuna devam etmek ister misiniz? (evet/hayır)");
             String playAgain = scanner.nextLine().toLowerCase();
             if (!playAgain.equals("evet")) {
                 break;
             }
         }
-        //Son puanı göster
+
+        // Son puanı göster
         System.out.println("Son puanınız: " + score);
         System.out.println("Oynadığınız için teşekkürler.");
     }
