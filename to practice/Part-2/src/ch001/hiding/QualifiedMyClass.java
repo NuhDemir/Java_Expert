3b6179e683ac191cd885a51d32baacee60ecad30
NuhDemir <drmaa217@gmail.com>
2024-07-23 18:56:31 +0300

    Qualified MyClass

---
+++ to practice/Part-2/src/ch001/hiding/QualifiedMyClass.java
@@ -0,0 +1,8 @@
+package ch001.hiding;
+
+public class QualifiedMyClass {
+    public static void main(String[] args) {
+        SingleImport singleImport = new SingleImport();
+        singleImport.add(1, 4);
+    }
+}
