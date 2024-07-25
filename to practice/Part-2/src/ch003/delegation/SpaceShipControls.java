f8561872153e44674acb95519549c7b3eb3f0707
NuhDemir <drmaa217@gmail.com>
2024-07-25 19:21:54 +0300

    delegation

---
+++ to practice/Part-2/src/ch003/delegation/SpaceShipControls.java
@@ -0,0 +1,39 @@
+package ch003.delegation;
+
+public class SpaceShipControls {
+    void up(int velocity) {
+    }
+
+    void down(int velocity) {
+    }
+
+    void left(int velocity) {
+    }
+
+    void right(int velocity) {
+    }
+
+    void forward(int velocity) {
+    }
+
+    void back(int velocity) {
+    }
+
+    void boost(int velocity) {
+    }
+
+    void fuel(int velocity) {
+    }
+
+    void tank(int velocity) {
+    }
+
+    void blank(int velocity) {
+    }
+
+    void stairs(int velocity) {
+    }
+
+    void carbon(int velocity) {
+    }
+}
