0d358e251371258c456064a1e51c777f7e5e6c40
NuhDemir <drmaa217@gmail.com>
2024-07-26 20:17:03 +0300

    Blank Final

---
+++ to practice/Part-2/src/ch004/the_final/blank_final/BlankFinal.java
@@ -0,0 +1,32 @@
+package ch004.the_final.blank_final;
+
+class Poppet {
+    private int i;
+
+    Poppet(int ii) {
+        i = ii;
+    }
+}
+
+public class BlankFinal {
+
+    private final int i = 0; //Initialized final
+    private final int j;//Blank final
+    private final Poppet p;//Blank final reference
+
+    public BlankFinal() {
+        j = 2;
+        p = new Poppet(1);
+    }
+
+    public BlankFinal(int x) {
+        j = x;//Initialize blank final
+        p = new Poppet(x);//Init blank final reference
+    }
+
+    public static void main(String[] args) {
+        new BlankFinal();
+        new BlankFinal(33);
+    }
+
+}
