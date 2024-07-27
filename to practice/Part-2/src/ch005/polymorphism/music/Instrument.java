39cb50ce94ae0d3a20e5daa974da3e4c7170e401
NuhDemir <drmaa217@gmail.com>
2024-07-27 15:30:27 +0300

    polymorphism

--- to practice/Part-2/src/ch005/polymorphism/music/Instrument.java
+++ to practice/Part-2/src/ch005/polymorphism/music/Instrument.java
@@ -2,7 +2,7 @@ package ch005.polymorphism.music;
 
 public class Instrument {
 
-    public void play(NoteExample note) {
+    public void play(Note note) {
         System.out.println("Instrument.play()");
     }
 }
