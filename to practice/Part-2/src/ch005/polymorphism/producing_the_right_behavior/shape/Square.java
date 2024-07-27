a35925493214468c5d16bfd8d275d1f137e9e1fb
NuhDemir <drmaa217@gmail.com>
2024-07-27 15:45:49 +0300

    producing the right behavior

---
+++ to practice/Part-2/src/ch005/polymorphism/producing_the_right_behavior/shape/Square.java
@@ -0,0 +1,15 @@
+package ch005.polymorphism.producing_the_right_behavior.shape;
+
+public class Square extends Shape {
+
+
+    @Override
+    public void draw() {
+        System.out.println("Square.draw()");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Square.erase()");
+    }
+}
