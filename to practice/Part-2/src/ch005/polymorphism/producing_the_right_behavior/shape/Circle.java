41bf82506a2fd6d32dbe0dc08f807690fce8c420
NuhDemir <drmaa217@gmail.com>
2024-07-27 15:45:15 +0300

    producing the right behavior

---
+++ to practice/Part-2/src/ch005/polymorphism/producing_the_right_behavior/shape/Circle.java
@@ -0,0 +1,13 @@
+package ch005.polymorphism.producing_the_right_behavior.shape;
+
+public class Circle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Circle.draw()");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Circle.erase()");
+    }
+}
