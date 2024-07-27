a1aca1e7771d486a790b876e5be80d223ca0cd5c
NuhDemir <drmaa217@gmail.com>
2024-07-27 15:52:54 +0300

    comment line to random shapes

--- to practice/Part-2/src/ch005/polymorphism/producing_the_right_behavior/shape/RandomShape.java
+++ to practice/Part-2/src/ch005/polymorphism/producing_the_right_behavior/shape/RandomShape.java
@@ -1,13 +1,16 @@
 package ch005.polymorphism.producing_the_right_behavior.shape;
 
+import org.w3c.dom.UserDataHandler;
+
 import java.util.Random;
 
 public class RandomShape {
 
-    private Random random = new Random(22);
+    private Random random = new Random();
 
+    // Rasgele bir Shape nesnesi döndüren yöntem
     public Shape get() {
-        switch (random.nextInt(2)) {
+        switch (random.nextInt(9)) {  // 0 ile 8 arasında rasgele bir sayı üretir.
             default:
             case 0:
                 return new Circle();
@@ -27,6 +30,24 @@ public class RandomShape {
                 return new Square();
             case 8:
                 return new Triangle();
+        }
+    }
+
+    // Rasgele Shape nesneleri içeren bir dizi döndüren yöntem
+    public Shape[] array(int size) {
+        Shape[] shapes = new Shape[size];  // Dizi boyutunu belirtirken hata düzeltilir.
+        for (int i = 0; i < size; i++) {  // Dizi boyutuna kadar döngü oluşturulur.
+            shapes[i] = get();  // Rasgele Shape nesneleri dizinin elemanlarına atanır.
+        }
+        return shapes;  // Diziyi döndür.
+    }
+
+    public static void main(String[] args) {
+        RandomShape randomShape = new RandomShape();
+        Shape[] shapesArray = randomShape.array(10);  // 10 elemanlı bir Shape dizisi oluşturulur.
+
+        for (Shape shape : shapesArray) {
+            shape.draw();  // Her bir Shape nesnesinin draw() yöntemi çağrılır.
         }
     }
 }
