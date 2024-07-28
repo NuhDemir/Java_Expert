5c599e0fd587e310453af159e20223aac1338ec4
NuhDemir <drmaa217@gmail.com>
2024-07-28 10:56:41 +0300

    add

--- projects/src/calculator_OOP/Add.java
+++ projects/src/calculator_OOP/Add.java
@@ -1,4 +1,14 @@
 package calculator_OOP;
 
-public class Add {
+public class Add implements Operated{
+
+    @Override
+   public Double getResult(Double... numbers) {
+        Double sum = 0.0;
+
+        for (Double num:  numbers) {
+            sum+=num;
+        }
+        return sum;
+    }
 }
