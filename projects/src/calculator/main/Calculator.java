5a0e2149a1da602ac5d68cfba09fb6d680fad321
NuhDemir <drmaa217@gmail.com>
2024-07-28 10:38:34 +0300

    calculator methods

--- projects/src/calculator/main/Calculator.java
+++ projects/src/calculator/main/Calculator.java
@@ -1,9 +1,13 @@
 package calculator.main;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.util.function.Consumer;
 
 public class Calculator {
-
+    //Tanımlama:
+//region
     private static final int WINDOW_WIDTH = 400;
     private static final int WINDOW_HEIGHT = 600;
     private static final int BUTTON_HEIGHT = 75;
@@ -19,16 +23,31 @@ public class Calculator {
             btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnPoint,
             btnEqual, btnRoot, btnPower, btnLog;
 
-    private char operator=' ';//Save the operator
-    private boolean go=true;//For calculate with operator !=(=)
+    private char operator = ' ';//Save the operator
+    private boolean go = true;//For calculate with operator !=(=)
     private boolean addWrite = true;//Connect numbers in display
-    private double val=0;//Save the value typed for calculation
+    private double val = 0;//Save the value typed for calculation
+//endregion
 
-
-    public Calculator(){
-        window= new JFrame("Calculator");
-        window.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+    public Calculator() {
+        window = new JFrame("Calculator");
+        window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         window.setLocationRelativeTo(null); //Move window to center
     }
 
+    public JComboBox<String> initCombo(String[] items, int x, int y, String toolTip, Consumer consumerEvent) {
+    }
+
+    private JButton initButton(String label, int x, int y, ActionListener event) {
+
+    }
+
+    private Consumer<ItemEvent> calcTypeSwitchEventConsumer = event -> {
+    }
+    private Consumer<ItemEvent> themeSwitchEventConsumer = event -> {
+    }
+
+    public static void main(String[] args) {
+        new Calculator();
+    }
 }
