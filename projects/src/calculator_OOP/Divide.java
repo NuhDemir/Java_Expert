e29410107ee40a44bab4dc9c599208222a0c8634
NuhDemir <drmaa217@gmail.com>
2024-07-28 10:56:49 +0300

    Divide

--- projects/src/calculator_OOP/Divide.java
+++ projects/src/calculator_OOP/Divide.java
@@ -1,4 +1,13 @@
 package calculator_OOP;
 
-public class Divide {
+public class Divide implements Operated {
+    @Override
+    public Double getResult(Double... numbers) {
+        Double div = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            div /= numbers[i];
+        }
+        return div;
+    }
 }
