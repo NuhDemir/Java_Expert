d8e8722a883e36f72b63a0ea0e1b8fa3f7e22c5a
NuhDemir <drmaa217@gmail.com>
2024-07-29 11:41:05 +0300

    ıf else 1

---
+++ hackerrank/Hackerrank/src/IfElse_1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class IfElse_1 {
+	   private static final Scanner scanner = new Scanner(System.in);
+
+	    public static void main(String[] args) {
+	        int N = scanner.nextInt();
+	   
+
+	        if (N % 2 != 0) {
+	            System.out.println("Weird");
+	        } else {
+	            if (N >= 2 && N <= 5) {
+	                System.out.println("Not Weird");
+	            } else if (N >= 6 && N <= 20) {
+	                System.out.println("Weird");
+	            } else {
+	                System.out.println("Not Weird");
+	            }
+	        }
+	    }
+}
