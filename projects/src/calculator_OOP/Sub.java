a1b65a95a7d72d9d67615b245caa3672f3adfbcc
NuhDemir <drmaa217@gmail.com>
2024-07-31 16:46:18 +0300

    Sub

--- projects/src/calculator_OOP/Sub.java
+++ projects/src/calculator_OOP/Sub.java
@@ -1,4 +1,13 @@
 package calculator_OOP;
 
-public class Sub {
+public class Sub  implements Operated{
+    @Override
+    public Double getResult(Double... numbers) {
+        Double sub = numbers[0];
+
+        for (int i = 1; i<numbers.length;i++){
+                  sub +=numbers[i];
+        }
+        return sub;
+    }
 }
