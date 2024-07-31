4fb24778cde6434d33312454fe88e2d1c2753ab6
NuhDemir <drmaa217@gmail.com>
2024-07-31 16:46:12 +0300

    ReadInput

--- projects/src/calculator_OOP/ReadInput.java
+++ projects/src/calculator_OOP/ReadInput.java
@@ -1,4 +1,15 @@
 package calculator_OOP;
 
+import java.util.Scanner;
+
 public class ReadInput {
+public static String read(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("input expression example");
+    String inputLine =scanner.nextLine();
+    scanner.close();
+
+    return inputLine;
+}
+
 }
