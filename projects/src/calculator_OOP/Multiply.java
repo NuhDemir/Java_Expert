c46b81d3884c1e82cb3b7622ffa4568bd5dbc64f
NuhDemir <drmaa217@gmail.com>
2024-07-31 16:46:06 +0300

    Multiply

--- projects/src/calculator_OOP/Multiply.java
+++ projects/src/calculator_OOP/Multiply.java
@@ -1,4 +1,12 @@
 package calculator_OOP;
 
-public class Multiply {
+public class Multiply  implements Operated{
+    @Override
+    public Double getResult(Double... numbers) {
+        Double multiply =1.0;
+        for (Double num:numbers){
+            multiply*=num;
+        }
+        return multiply;
+    }
 }
