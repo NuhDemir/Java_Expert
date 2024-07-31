97628cd2292b3dee48c8bdb68a435b0551663882
NuhDemir <drmaa217@gmail.com>
2024-07-31 16:45:57 +0300

    Mod

--- projects/src/calculator_OOP/Mod.java
+++ projects/src/calculator_OOP/Mod.java
@@ -1,4 +1,12 @@
 package calculator_OOP;
 
-public class Mod {
+public class Mod implements Operated{
+    @Override
+    public Double getResult(Double... numbers) {
+        Double mod =numbers[0];
+        for (int i=1;i<numbers.length;i++){
+            mod%=numbers[i];
+        }
+        return mod;
+    }
 }
