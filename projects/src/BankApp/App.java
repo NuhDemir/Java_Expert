f54ea9f9e52a65d4d84b8b99be18c7a8f97b048f
NuhDemir <drmaa217@gmail.com>
2024-08-01 16:30:20 +0300

    bank app

---
+++ projects/src/BankApp/App.java
@@ -0,0 +1,27 @@
+package BankApp;
+
+import java.time.LocalDateTime;
+
+public class App extends Bank {
+
+    public static void main(String[] args) {
+        Account account1 = new Account("Ali", "12345", "Ali Saygın", 2000.100);
+
+
+        account1.viewBalance();
+
+        account1.deposit(200);
+        account1.withdraw(1200);
+        account1.withdraw(0.0999999999999);
+        account1.hashCode();
+        System.out.println("account 1 hashCode(): " + account1.hashCode());
+   System.out.println("Account 1 getClass(): "+account1.getClass());
+   System.out.println(account1);
+System.out.println("------------------------");
+
+Bank.transactionHistory.clear();
+   Bank.displayInfo();
+   Bank.logTransaction("message");
+   Bank.printTransactions();
+    }
+}
