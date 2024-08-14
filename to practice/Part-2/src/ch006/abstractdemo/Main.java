f2339b3baf5371bbd434fd30ef57fd965360d96d
NuhDemir <drmaa217@gmail.com>
2024-08-14 19:18:08 +0300

    main

---
+++ to practice/Part-2/src/ch006/abstractdemo/Main.java
@@ -0,0 +1,10 @@
+package ch006.abstractdemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+CustomerManager customerManager = new CustomerManager();
+customerManager.databaseManager = new SqlServerDatabaseManager();
+customerManager.getCustomers();
+    }
+}
