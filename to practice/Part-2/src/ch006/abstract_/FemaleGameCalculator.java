8f076fd770dc8bfdce986b15432a3890c452ea9e
NuhDemir <drmaa217@gmail.com>
2024-08-14 18:38:54 +0300

    calculate

---
+++ to practice/Part-2/src/ch006/abstract_/FemaleGameCalculator.java
@@ -0,0 +1,10 @@
+package ch006.abstract_;
+
+public class FemaleGameCalculator extends GameCalcutator {
+
+
+    @Override
+    public double calculate(double point) {
+        return 100 * point;
+    }
+}
