f3a29ff2e4c0fe9927929d195bd3c776ae3cb4cd
NuhDemir <drmaa217@gmail.com>
2024-08-14 18:37:42 +0300

    Log

---
+++ to practice/Part-2/src/ch005/polymorphism/logDemo/Main.java
@@ -0,0 +1,13 @@
+package ch005.polymorphism.logDemo;
+
+public class Main {
+    public static void main(String[] args) {
+//       BaseLogger[] loggers = new BaseLogger[]{new FileLogger(),new EmailLogger(),new DatabaseLogger(),new ConsoleLogger()};
+//       for (BaseLogger logger:loggers){
+//           logger.Log("Log message");
+//       }
+
+        CustomerManager customerManager = new CustomerManager(new FileLogger());
+        customerManager.Add();
+    }
+}
